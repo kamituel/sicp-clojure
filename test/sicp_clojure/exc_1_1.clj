184f3ae6f7a841a9084d482feeb2a00931e49b53
André Drougge <andre.drougge@gmail.com>
2015-03-24 17:50:26 +0200

    No camelCase in Clojure.

--- test/sicp_clojure/exc_1_1.clj
+++ test/sicp_clojure/exc_1_1.clj
@@ -5,23 +5,23 @@
 (def a 3)
 (def b (+ a 1))
 
-(deftest tenIsTen
+(deftest ten-is-ten
   (is (= 10 10)))
 
-(deftest flatAddition
+(deftest flat-addition
   (is (= 12 (+ 5 3 4))))
 
-(deftest flatSubtraction
+(deftest flat-subtraction
   (is (= 8 (- 9 1))))
 
 (deftest division
   (is (= 3 (/ 6 2))))
 
-(deftest mathMix
+(deftest math-mix
   (is (= 6
     		 (+ (* 2 4) (- 4 6)))))
 
-(deftest combinationAndConditions
+(deftest combination-and-conditions
 
   (is (= 19
     		 (+ a b (* a b))))
